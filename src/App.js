01632fbd4f4b2ab7239e29df8f5305f61f9b708a
Vishnu Vardhan <vishnuthecoder@gmail.com>
2020-10-25 17:54:54 +0530

    Fifth Commit: Modified App.js to a function-based component(useState using React Hooks)

--- src/App.js
+++ src/App.js
@@ -1,31 +1,57 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Person from './Person/Person';
 
-class App extends Component {
+const app = (props) => {
+	const [ personState, personStateHandler ] = useState({
+		persons: [
+			{ name: 'Vishnu', age: 22 },
+			{ name: 'Hina', age: 21 },
+			{ name: 'Shivendu', age: 23 },
+			{ name: 'JAVA', age: 25 }
+		]
+	});
+
+	const [ otherState, otherStateHandler ] = useState({ otherStuff: 'Nothing special here!' });
+
+	console.log(personState, otherState);
+
+	/*
+  Note that here 'otherstuff' won't be visible when the state changes as 
+  useState doesn't merge the changes with the previous state.
+
+  Therefore in order to overcome that we can use multiple useState splices 
+  to store them:
   
-  state = {
-    persons: [
-      {name: 'Vishnu', age: 22},
-      {name: 'Hina', age: 21},
-      {name: 'Shivendu', age: 23},
-      {name: 'JAVA', age: 25}
-    ]
-  }
+  const [ otherState, otherStateHandler ] = useState({ otherStuff: 'Nothing special here!' });
 
-  render() {
-    return (
-      <div class="App">        
-        <h1>Comaecod's React Application</h1>
-        <p>This is cool!</p>
-        <button>Switch Name</button>
-        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
-        <Person name={this.state.persons[1].name} age={this.state.persons[1].age} />
-        <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
-        <Person name={this.state.persons[3].name} age={this.state.persons[3].age} />
-      </div>  
-    );
-  }
-}
+  rather than manually add the previous state to the new state:
+  
+  otherStuff: personState.otherStuff
+  */
 
-export default App;
+	const switchNameHandler = () => {
+		personStateHandler({
+			persons: [
+				{ name: 'Vishnu Vardhan', age: 22 },
+				{ name: 'Hina', age: 21 },
+				{ name: 'Shivendu', age: 23 },
+				{ name: 'JAVA', age: 35 }
+			]
+		});
+	};
+
+	return (
+		<div className="App">
+			<h1>Comaecod's React Application</h1>
+			<p>This is cool!</p>
+			<button onClick={switchNameHandler}>Switch Name</button>
+			<Person name={personState.persons[0].name} age={personState.persons[0].age} />
+			<Person name={personState.persons[1].name} age={personState.persons[1].age} />
+			<Person name={personState.persons[2].name} age={personState.persons[2].age} />
+			<Person name={personState.persons[3].name} age={personState.persons[3].age} />
+		</div>
+	);
+};
+
+export default app;
