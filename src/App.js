df4fbf87b03f52721ab69b9f1f5506a2636735e1
Vishnu Vardhan <vishnuthecoder@gmail.com>
2020-10-30 16:54:18 +0530

    Sixth Commit: Added a Person.css file and changed back to class-based app component

--- src/App.js
+++ src/App.js
@@ -1,39 +1,29 @@
-import React, { useState } from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import Person from './Person/Person';
 
-const app = (props) => {
-	const [ personState, personStateHandler ] = useState({
-		persons: [
+class App extends Component {
+	state = {
+		persons : [
 			{ name: 'Vishnu', age: 22 },
 			{ name: 'Hina', age: 21 },
 			{ name: 'Shivendu', age: 23 },
 			{ name: 'JAVA', age: 25 }
 		]
-	});
+	};
 
-	const [ otherState, otherStateHandler ] = useState({ otherStuff: 'Nothing special here!' });
+	styleObj = {
+		backgroundColor : 'white',
+		font            : 'inherit',
+		border          : '2px solid red',
+		padding         : '8px',
+		cursor          : 'pointer'
+	};
 
-	console.log(personState, otherState);
-
-	/*
-  Note that here 'otherstuff' won't be visible when the state changes as 
-  useState doesn't merge the changes with the previous state.
-
-  Therefore in order to overcome that we can use multiple useState splices 
-  to store them:
-  
-  const [ otherState, otherStateHandler ] = useState({ otherStuff: 'Nothing special here!' });
-
-  rather than manually add the previous state to the new state:
-  
-  otherStuff: personState.otherStuff
-  */
-
-	const switchNameHandler = () => {
-		personStateHandler({
-			persons: [
-				{ name: 'Vishnu Vardhan', age: 22 },
+	switchNameHandler = (newName) => {
+		this.setState({
+			persons : [
+				{ name: newName, age: 22 },
 				{ name: 'Hina', age: 21 },
 				{ name: 'Shivendu', age: 23 },
 				{ name: 'JAVA', age: 35 }
@@ -41,17 +31,38 @@ const app = (props) => {
 		});
 	};
 
-	return (
-		<div className="App">
+	nameChangedHandler = (event) => {
+		this.setState({
+			persons : [
+				{ name: event.target.value, age: 22 },
+				{ name: 'Hina', age: 21 },
+				{ name: 'Shivendu', age: 23 },
+				{ name: 'JAVA', age: 35 }
+			]
+		});
+	};
+
+	render = () => (
+		<div className='App'>
 			<h1>Comaecod's React Application</h1>
 			<p>This is cool!</p>
-			<button onClick={switchNameHandler}>Switch Name</button>
-			<Person name={personState.persons[0].name} age={personState.persons[0].age} />
-			<Person name={personState.persons[1].name} age={personState.persons[1].age} />
-			<Person name={personState.persons[2].name} age={personState.persons[2].age} />
-			<Person name={personState.persons[3].name} age={personState.persons[3].age} />
+			<button style={this.styleObj} onClick={this.switchNameHandler.bind(this, 'Comaecod')}>
+				Switch Name
+			</button>
+			<Person
+				name={this.state.persons[0].name}
+				age={this.state.persons[0].age}
+				changed={this.nameChangedHandler}
+			/>
+			<Person
+				name={this.state.persons[1].name}
+				age={this.state.persons[1].age}
+				click={() => this.switchNameHandler('ComaecodNew')}
+			/>
+			<Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
+			<Person name={this.state.persons[3].name} age={this.state.persons[3].age} />
 		</div>
 	);
-};
+}
 
-export default app;
+export default App;
